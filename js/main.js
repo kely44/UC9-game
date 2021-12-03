8dd1ea406cb2dd91c728c3878dc5a03586b9f37d
kely.silva <kelysantana@hotmail.com>
2021-12-02 23:47:37 -0300

    js

---
+++ js/main.js
@@ -0,0 +1,9 @@
+$('.slider-principal').slick({
+    dots: true,
+    Infinite: true,
+    Speed: 300,
+    slidestoshow: 4,
+    adaptiveheight: true,
+    autoplay: true,
+    autoplayspeed: 2000
+});
