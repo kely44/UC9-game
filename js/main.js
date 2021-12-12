495c13284365489b7d998173cada4aebddd39610
kely.silva <kelysantana@hotmail.com>
2021-12-12 17:41:34 -0300

    serviços

--- js/main.js
+++ js/main.js
@@ -7,3 +7,12 @@ $('.slider-principal').slick({
     autoplay: true,
     autoplayspeed: 2000
 });
+function Enviar() {
+
+    var nome = document.getElementById("nomeid");
+
+    if (nome.value != "") {
+        alert('Obrigado sr(a) ' + nome.value + ' os seus dados foram encaminhados com sucesso');
+    }
+
+}
